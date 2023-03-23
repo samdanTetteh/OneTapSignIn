facf9d0254c52a95260f0007a27dc418c5e74068
Samdan Kudjodji <samdantettey@gmail.com>
2023-03-23 19:12:52 +0000

    Handle user's response

--- app/src/main/java/com/ijikod/onetapsignin/MainActivity.kt
+++ app/src/main/java/com/ijikod/onetapsignin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ijikod.onetapsignin
 
+import android.content.Intent
 import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,11 +8,15 @@ import android.util.Log
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var oneTapClient: SignInClient
     private lateinit var signInClient: BeginSignInRequest
+
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private var showOneTapUI = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,11 +50,11 @@ class MainActivity : AppCompatActivity() {
             .addOnSuccessListener {
                 try {
                     startIntentSenderForResult(
-                        it.pendingIntent.intentSender, 1, null, 0,
+                        it.pendingIntent.intentSender, REQ_ONE_TAP, null, 0,
                         0, 0, null
                     )
                 }catch (e: IntentSender.SendIntentException){
-                    Log.e("OnTap", "Couldn't start One Tap UI: ${e.localizedMessage}"))
+                    Log.e("ONE TAP", "Couldn't start One Tap UI: ${e.localizedMessage}"))
                 }
 
             }
@@ -57,7 +62,43 @@ class MainActivity : AppCompatActivity() {
             .addOnFailureListener {
                 // No saved credentials found. Launch the One Tap sign-up flow, or
                 // do nothing and continue presenting the signed-out UI.
-                Log.d(TAG, e.localizedMessage)
+                it.localizedMessage?.let { errorMsg ->
+                    Log.d("ONE TAP", errorMsg)
+                }
             }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    val username = credential.id
+                    val password = credential.password
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with your backend.
+                            Log.d("ONE TAP", "Got ID token.")
+                        }
+                        password != null -> {
+                            // Got a saved username and password. Use them to authenticate
+                            // with your backend.
+                            Log.d("ONE TAP", "Got password.")
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d("ONE TAP", "No ID token or password!")
+                        }
+                    }
+                } catch (_ : ApiException) {
+
+                }
+            }
+        }
     }
 }
