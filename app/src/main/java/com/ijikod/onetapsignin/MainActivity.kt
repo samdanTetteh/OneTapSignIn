33a124a7a15f74a9fa5ff9871c17623d7391cec2
Samdan Kudjodji <samdantettey@gmail.com>
2023-03-23 19:07:24 +0000

    Display On tap sign-in UI

--- app/src/main/java/com/ijikod/onetapsignin/MainActivity.kt
+++ app/src/main/java/com/ijikod/onetapsignin/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ijikod.onetapsignin
 
+import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpOnTapSignInClient()
+        displayOneTapSignInUI()
+
+    }
+
+
+    private fun setUpOnTapSignInClient() {
         oneTapClient = Identity.getSignInClient(this)
         signInClient = BeginSignInRequest.builder()
             .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder().setSupported(true).build())
@@ -28,5 +37,27 @@ class MainActivity : AppCompatActivity() {
             // Automatically sign in when exactly one credential is retrieved.
             .setAutoSelectEnabled(true)
             .build()
+    }
+
+
+    private fun displayOneTapSignInUI(){
+        oneTapClient.beginSignIn(signInClient)
+            .addOnSuccessListener {
+                try {
+                    startIntentSenderForResult(
+                        it.pendingIntent.intentSender, 1, null, 0,
+                        0, 0, null
+                    )
+                }catch (e: IntentSender.SendIntentException){
+                    Log.e("OnTap", "Couldn't start One Tap UI: ${e.localizedMessage}"))
+                }
+
+            }
+
+            .addOnFailureListener {
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+                Log.d(TAG, e.localizedMessage)
+            }
     }
 }
